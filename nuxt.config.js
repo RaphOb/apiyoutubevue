dcb4fce0c9ed74d12146c456e14016493a18216f
Raph <obadia_r@etna-alternance.net>
2019-05-24 13:59:03 +0200

    first commit

---
+++ nuxt.config.js
@@ -0,0 +1,51 @@
+module.exports = {
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'youtubevue',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Nuxt.js project' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /**
+   * Axios
+   */
+  modules: [
+    '@nuxtjs/axios',
+  ],
+
+  axios: {
+    // proxyHeaders: false
+    baseURL: 'http://192.168.197.131:8080/'
+  },
+
+  /*
+  ** Customize the progress bar color
+  */
+  loading: { color: '#3B8070' },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** Run ESLint on save
+    */
+    extend (config, { isDev, isClient }) {
+      if (isDev && isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/
+        })
+      }
+    }
+  }
+}
+
---
+++ pages/user.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>{{ users}}</h1>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    asyncData ({ params }) {
+      return axios.get(`http://192.168.197.131:8080/api/users`)
+        .then((res) => {
+          return { users: res.data }
+        })
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
